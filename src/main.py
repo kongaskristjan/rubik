d5ba8339025eca179f158c90214d4e339333191d
Kristjan Kongas <kongaskristjan@gmail.com>
2020-04-24 14:55:01 +0300

    Add multiepoch training and better stats

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 def main():
     tfms = Compose([data.CubeToIndices(), data.IndicesToOneHot()])
-    ds = data.RubikDataset(600000, 20, tfms=tfms)
+    ds = data.RubikDataset(60000, 5, tfms=tfms)
     dl = DataLoader(ds, batch_size=64, num_workers=16)
 
     net = models.DeepCube().to(device)
@@ -18,15 +18,20 @@ def main():
 
 def train(net, dl):
     optim = torch.optim.Adam(net.parameters())
-    criterion = utils.CubeLoss().to(device)
-    for input, target in dl:
-        optim.zero_grad()
-        input, target = input.to(device), target.to(device)
-        output = net(input)
-        loss = criterion(output, target)
-        loss.backward()
-        optim.step()
-        print(loss.item())
+    criterion = utils.CubeLoss('sum').to(device)
+
+    epochs = 10
+    for e in range(1, epochs + 1):
+        stats = utils.Stats()
+        for input, target in dl:
+            optim.zero_grad()
+            input, target = input.to(device), target.to(device)
+            output = net(input)
+            loss, acc = criterion(output, target)
+            loss.backward()
+            optim.step()
+            stats.accumulate(len(target), loss, acc)
+        print(f'Epoch {e}/{epochs}: acc={100*stats.getAcc():.2f}%, loss={stats.getLoss():.3f}')
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,36 @@ from torch import nn
 from torch.nn import functional as F
 
 class CubeLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, reduction='mean'):
         super(CubeLoss, self).__init__()
-        self.criterion = nn.CrossEntropyLoss()
+        self.criterion = nn.CrossEntropyLoss(reduction=reduction)
+        self.reduction = reduction
 
     def forward(self, output, target):
         output = output.view(output.shape[0], -1) # NSC -> N(SC)
         target = 3 * target[:, 0] + target[:, 1]
+
         loss = self.criterion(output, target)
-        return loss
+
+        pred = torch.argmax(output, dim=1)
+        if self.reduction == 'mean': acc = torch.mean(pred == target)
+        if self.reduction == 'sum': acc = torch.sum(pred == target)
+        return loss, acc
+
+
+class Stats:
+    def __init__(self):
+        self.n = 0
+        self.loss = 0
+        self.acc = 0
+
+    def accumulate(self, _n, _loss, _acc):
+        self.n += _n
+        self.loss += _loss.item()
+        self.acc += _acc.item()
+
+    def getLoss(self):
+        return self.loss / max(1, self.n)
+
+    def getAcc(self):
+        return self.acc / max(1, self.n)
