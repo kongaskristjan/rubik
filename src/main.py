ae391430c5dfffe1bc373c6bc6169897cb966e83
Kristjan Kongas <kongaskristjan@gmail.com>
2020-04-25 13:47:59 +0300

    Add command line arguments with fire

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import torch, os
+import torch, os, fire
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 import data, models, utils
@@ -9,18 +9,21 @@ device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 maxScrambles = 20
 modelDir = '../models'
 
-def main():
+def main(epochs=10, load_path=''):
     ds = data.RubikDataset(60000, maxScrambles)
     dl = DataLoader(ds, batch_size=64, num_workers=16)
 
-    net = models.DeepCube().to(device)
-    train(net, dl)
+    if load_path == '':
+        net = models.DeepCube().to(device)
+        train(net, dl, epochs)
+    else:
+        net = torch.load(load_path).to(device)
+        testMulti(net, 20)
 
-def train(net, dl):
+def train(net, dl, epochs):
     optim = torch.optim.Adam(net.parameters())
     criterion = utils.CubeLoss('none').to(device)
 
-    epochs = 10
     for e in range(1, epochs + 1):
         stats = utils.Stats()
         perClass = utils.PerClassStats(maxScrambles)
@@ -85,4 +88,4 @@ def testSolve(net, scrambles=100):
 
 
 if __name__ == '__main__':
-    main()
+    fire.Fire(main)
