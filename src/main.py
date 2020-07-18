a8d89880871e55b20f9da4d89bf0f3de3a12fd94
Kristjan Kongas <kongaskristjan@gmail.com>
2020-07-18 17:36:27 +0300

    Refactor deepcube model to sequential

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def main(start_epoch=0, end_epoch=1000, save_frequency=50, mode='train'):
     dl = DataLoader(ds, batch_size=64, num_workers=16)
 
     if start_epoch == 0:
-        net = models.DeepCube(mul=2).to(device)
+        net = models.getModel(mul=2).to(device)
     else:
         net = torch.load(getModelPath(start_epoch), map_location=device)
 
--- src/models.py
+++ src/models.py
@@ -3,20 +3,19 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-# Closely mimics DeepCube from SOLVING THE RUBIK’S CUBE WITH APPROXIMATE POLICY ITERATION
-class DeepCube(nn.Module):
-    def __init__(self, mul=1):
-        super(DeepCube, self).__init__()
-        self.linear1 = nn.Linear(6 * 6 * 3 * 3, round(mul * 4096))
-        self.linear2 = nn.Linear(round(mul * 4096), round(mul * 2048))
-        self.linear3 = nn.Linear(round(mul * 2048), round(mul * 512))
-        self.linear4 = nn.Linear(round(mul * 512), 6 * 2)
-
+class Flatten(nn.Module):
     def forward(self, x):
-        x = x.reshape(x.shape[0], -1)
-        x = F.relu(self.linear1(x), inplace=True)
-        x = F.relu(self.linear2(x), inplace=True)
-        x = F.relu(self.linear3(x), inplace=True)
-        x = self.linear4(x)
-        x = x.reshape(x.shape[0], 6, 2)
-        return x
+        return x.view(x.shape[0], -1)
+
+
+def getModel(mul=1):
+    return nn.Sequential(
+        Flatten(),
+        nn.Linear(6 * 6 * 3 * 3, round(mul * 4096)),
+        nn.ReLU(inplace=True),
+        nn.Linear(round(mul * 4096), round(mul * 2048)),
+        nn.ReLU(inplace=True),
+        nn.Linear(round(mul * 2048), round(mul * 512)),
+        nn.ReLU(inplace=True),
+        nn.Linear(round(mul * 512), 6 * 2),
+    )
